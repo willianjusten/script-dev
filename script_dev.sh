b99d3d9b07e2a0f826723ceaaf5abb82ec6f8ac2
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-06 23:52:03 -0300

    terminator

--- script_dev.sh
+++ script_dev.sh
@@ -3,44 +3,59 @@
 # Script para desenvolvedores web no Ubuntu
 # Criado por Willian Justen
 
-# Esse script foi escrito usando o zenity e tem 
+# Esse script foi escrito usando o zenity e tem
 # o objetivo de agilizar a instalação de programas
 # básicos no mundo do desenvolvimento web atual.
 # É um script bem básico e rápido, sugestões são bem vindas =)
 
 zenity --info --title "Script Web Development" --text "Este é um script simples para instalação de ferramentas úteis para Desenvolvimento Web e do ambiente do Ubuntu. \n\nCriado por: Willian Justen de Vasconcellos"
 
-resposta=$(zenity  --list  --text "Escolha os pacotes que deseja instalar." --checklist  --column "Instalar" --column "Id" --column "Pacote" FALSE "a" "Restricted-Extras (Codecs de áudio e vídeo, plugin flash, java entre outros)" FALSE "b" "Gimp (Editor de Imagens)" FALSE "c" "Inkscape (Editor de Vetores)" FALSE "d" "Imagemagick (Editor de Imagens via Terminal)" FALSE "e" "Sublime Text 2" FALSE "f" "Sublime Text 3" FALSE "g" "Brackets" FALSE "h" "Dropbox" FALSE "i" "Mysql Workbench" FALSE "j" "Lamp+phpmyadmin" FALSE "k" "Google Chromium" FALSE "l" "Google Chrome" FALSE "m" "Git e Gitk" FALSE "n" "Filezilla" FALSE "o" "NodeJS" FALSE "p" "Yo / GruntJs / Bower (Requer NodeJS)" --separator=":" --width=750 --height=600)
+resposta=$(zenity  --list  --text "Escolha os pacotes que deseja instalar." --checklist  --column "Instalar" --column "Id" --column "Pacote"\
+    FALSE "a" "Restricted-Extras (Codecs de áudio e vídeo, plugin flash, java entre outros)"\
+    FALSE "b" "Gimp (Editor de Imagens)"\
+    FALSE "c" "Inkscape (Editor de Vetores)"\
+    FALSE "d" "Imagemagick (Editor de Imagens via Terminal)"\
+    FALSE "terminator" "Terminator"\
+    FALSE "f" "Sublime Text 3"\
+    FALSE "g" "Brackets"\
+    FALSE "h" "Dropbox"\
+    FALSE "i" "Mysql Workbench"\
+    FALSE "j" "Lamp+phpmyadmin"\
+    FALSE "k" "Google Chromium"\
+    FALSE "l" "Google Chrome"\
+    FALSE "m" "Git e Gitk"\
+    FALSE "n" "Filezilla"\
+    FALSE "o" "NodeJS"\
+    FALSE "p" "Yo / GruntJs / Bower (Requer NodeJS)" --separator=":" --width=750 --height=600)
 
 
 if [[ $resposta =~ "a" ]]; then
-   sudo apt-get -y install ubuntu-restricted-extras
+    sudo apt-get -y install ubuntu-restricted-extras
 fi
 
 
 if [[ $resposta =~ "b" ]]; then
-   sudo apt-get -y install gimp
+    sudo apt-get -y install gimp
 fi
 
 if [[ $resposta =~ "c" ]]; then
-   sudo apt-get -y install inkscape
+    sudo apt-get -y install inkscape
 fi
 
 if [[ $resposta =~ "d" ]]; then
    sudo apt-get -y install imagemagick
 fi
 
-if [[ $resposta =~ "e" ]]; then
-  # adicionando repositório
-  sudo add-apt-repository ppa:webupd8team/sublime-text-2
-  sudo apt-get update
-  sudo apt-get -y install sublime-text
+if [[ $resposta =~ "terminator" ]]; then
+    sudo add-apt-repository -y ppa:gnome-terminator
+    sudo apt-get update
+    sudo apt-get install terminator
 fi
 
 if [[ $resposta =~ "f" ]]; then
   # adicionando repositório
   sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-  sudo apt-get update 
+  sudo apt-get update
   sudo apt-get -y install sublime-text-installer
 fi
 
@@ -55,11 +70,11 @@ if [[ $resposta =~ "h" ]]; then
    sudo apt-get -y install nautilus-dropbox
  #instalando indicador da barra de tarefas
  sudo apt-get -y install libappindicator1
-fi   
+fi
 
 if [[ $resposta =~ "i" ]]; then
    sudo apt-get -y install mysql-workbench
-fi   
+fi
 
 if [[ $resposta =~ "j" ]]; then
   # instalando lamp e phpmyadmin
@@ -68,12 +83,12 @@ if [[ $resposta =~ "j" ]]; then
   # criando um link virtual
   sudo ln -s /usr/share/phpmyadmin/ /var/www/
   # dando permissão de todas as pastas para maior controle
-  sudo chmod -R 777 /var/www   
-fi   
+  sudo chmod -R 777 /var/www
+fi
 
 if [[ $resposta =~ "k" ]]; then
    sudo apt-get -y install chromium-browser
-fi   
+fi
 
 if [[ $resposta =~ "l" ]]; then
   # baixando a chave do chrome para adicionar nas chaves de repositórios
@@ -82,7 +97,7 @@ if [[ $resposta =~ "l" ]]; then
   sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
   sudo apt-get update
   sudo apt-get install -y google-chrome-stable
-fi   
+fi
 
 if [[ $resposta =~ "m" ]]; then
   sudo apt-get install -y git-core gitk
@@ -101,7 +116,7 @@ if [[ $resposta =~ "o" ]]; then
   sudo apt-get install -y nodejs
   # correção para não precisar rodar em sudo
   echo prefix = ~/.node >> ~/.npmrc
-  echo 'export PATH=$HOME/.node/bin:$PATH' >> ~/.bashrc 
+  echo 'export PATH=$HOME/.node/bin:$PATH' >> ~/.bashrc
 fi
 
 if [[ $resposta =~ "p" ]]; then
